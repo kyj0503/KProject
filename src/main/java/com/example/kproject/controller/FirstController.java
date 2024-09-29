3db367a64921e6383c372c29fc202ce7bba9a70c
kyj0503 <heroria0503@gmail.com>
2024-09-29 18:01:54 +0900

    K-프로젝트 시작

---
+++ src/main/java/com/example/kproject/controller/FirstController.java
@@ -0,0 +1,25 @@
+package com.example.kproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "지팡이");
+        return "greetings";
+    }
+
+    @GetMapping("/map")
+    public String mapDisplay(Model model) {
+        return "map";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "지팡이");
+        return "goodbye";
+    }
+}
